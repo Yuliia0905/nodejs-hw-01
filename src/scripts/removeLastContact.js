7e106f6f85e28b32513672d6173f414a9363a6f9
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-09-29 17:36:49 +0300

    HW-1 Node.js

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    contacts.splice(-1, 1);
+    //   contacts.pop()
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log('Error removing the last contact:', error);
+  }
+};
 
 removeLastContact();
